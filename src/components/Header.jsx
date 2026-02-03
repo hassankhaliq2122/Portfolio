0fcdd9babf0e3d763b2216b2c0d705489037b981
Codes <hassankhaliq2122@gmail.com>
2026-02-03 20:16:45 +0500

    asd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import headerLogo from "../assets/logo/Header.svg";
 import "./Header.css";
 import ArrowButton from "./ArrowButton";
@@ -6,41 +6,52 @@ import FullPageMenu from "./FullPageMenu";
 import { AnimatedThemeToggler } from "./ui/AnimatedThemeToggler";
 
 const Header = () => {
+  const [hidden, setHidden] = useState(false);
+  const lastScrollY = useRef(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY.current && currentScrollY > 100) {
+        // scrolling down
+        setHidden(true);
+      } else {
+        // scrolling up
+        setHidden(false);
+      }
+
+      lastScrollY.current = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <>
-      <div className="header">
-        <div className="headerContainer">
-          <header>
-            <img
-              src={headerLogo}
-              className="header-logo-container"
-              style={{ width: "166px", height: "52px" }}
-              alt="Header Logo"
-            />
-          </header>
-          <div className="headerLinks">
-            <ul>
-              <li>
-                <p className="headerText">Let's brand up your website</p>
-              </li>
-              <AnimatedThemeToggler className="text-gray-700 dark:text-gray-200" />
-              <ArrowButton text="Book a call" />
-              <FullPageMenu />
-            </ul>
-          </div>
+    <div className={`header ${hidden ? "hide" : ""}`}>
+      <div className="headerContainer">
+        <header>
+          <img
+            src={headerLogo}
+            className="header-logo-container"
+            style={{ width: "166px", height: "52px" }}
+            alt="Header Logo"
+          />
+        </header>
+
+        <div className="headerLinks">
+          <ul>
+            <li>
+              <p className="headerText">Let's brand up your website</p>
+            </li>
+            <AnimatedThemeToggler className="text-gray-700 dark:text-gray-200" />
+            <ArrowButton text="Book a call" />
+            <FullPageMenu />
+          </ul>
         </div>
       </div>
-      <hr
-        style={{
-          width: "100%",
-          height: "1px",
-          color: "#ffffffff",
-          margin: "12px 0",
-          border: "none",
-          backgroundColor: "#ffffffff",
-        }}
-      />
-    </>
+    </div>
   );
 };
 
